e3c718ba08a97582649b3a369bf73633506e6214
Nicolas Schneider <nicolas06062000@hotmail.com>
2019-01-25 23:14:46 +0100

    Set animation for player with spine

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,13 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Spine.Unity;
 using UnityEngine;
 using UnityEngine.Experimental.PlayerLoop;
 
 public class PlayerController : MonoBehaviour
 {
-
     //Movements
     [SerializeField] float movementSpeed;
+
+    SkeletonAnimation skeleton;
 
     Rigidbody2D body;
     Vector2 movementDirection;
@@ -16,6 +18,7 @@ public class PlayerController : MonoBehaviour
 
     void Start()
     {
+        skeleton = GetComponentInChildren<SkeletonAnimation>();
         body = GetComponent<Rigidbody2D>();
     }
     
@@ -27,6 +30,8 @@ public class PlayerController : MonoBehaviour
         movementDirection.x = Input.GetAxis("Horizontal");
         movementDirection.y = Input.GetAxis("Vertical");
 
+        UpdateAnimation();
+
         if (Input.GetButtonDown("Fire1")) {
             OverworldManager.Instance.GrabObject();
         }
@@ -37,6 +42,34 @@ public class PlayerController : MonoBehaviour
         //Movement
         body.velocity = movementDirection * movementSpeed;
         movementDirection = Vector2.zero;
+    }
+
+    void UpdateAnimation()
+    {
+        skeleton.transform.localScale = new Vector3(1, 1, 1);
+
+        if (Mathf.Abs(body.velocity.y) < 0.1f && Mathf.Abs(body.velocity.x) < 0.1f) {
+
+            skeleton.AnimationName = "idle";
+            return;
+        }
+
+        if(Mathf.Abs(body.velocity.x) > 0.1f)
+        {
+            skeleton.AnimationName = "walkside";
+            if(body.velocity.x < 0) {
+                skeleton.transform.localScale = new Vector3(-1, 1, 1);
+            }
+            return;
+        } else {
+            if(body.velocity.y < 0) {
+                skeleton.AnimationName = "walkdown";
+            } else {
+                skeleton.AnimationName = "walkup";
+            }
+
+            return;
+        }
     }
 
     public void Lock()
