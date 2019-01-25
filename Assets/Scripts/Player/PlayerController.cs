a1ec12bd7b526eeb8df6662c4a037f94e4b7d3d4
Nicolas Schneider <nicolas06062000@hotmail.com>
2019-01-25 18:41:20 +0100

    Basic player & add cinemachine

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.PlayerLoop;
+
+public class PlayerController : MonoBehaviour
+{
+
+    //Movements
+    [SerializeField] float movementSpeed;
+
+    Rigidbody2D body;
+    Vector2 movementDirection;
+
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+    
+    void Update()
+    {
+        movementDirection.x = Input.GetAxis("Horizontal");
+        movementDirection.y = Input.GetAxis("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        body.velocity = movementDirection * movementSpeed;
+        Debug.Log(body.velocity);
+        movementDirection = Vector2.zero;
+    }
+}
