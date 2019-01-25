d613c8da649a3306744ad9e898715e9e69a3baca
Nicolas Schneider <nicolas06062000@hotmail.com>
2019-01-25 18:51:26 +0100

    Add basic spaceship for planet movement

---
+++ Assets/Scripts/Spaceship/Spaceship.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spaceship : MonoBehaviour
+{
+    //Object to follow
+    [SerializeField] Transform objectToFollow;
+    [SerializeField] float heightOffset = 10;
+    [SerializeField] float lerpSpeed = 5;
+    
+    void Start()
+    {
+        transform.position = new Vector2(objectToFollow.transform.position.x, objectToFollow.transform.position.y + heightOffset);
+    }
+    
+    void Update()
+    {
+        Vector2 desiredPosition = new Vector2(objectToFollow.transform.position.x, objectToFollow.transform.position.y + heightOffset);
+        transform.position = Vector2.Lerp(transform.position, desiredPosition, Time.deltaTime * lerpSpeed);
+    }
+}
