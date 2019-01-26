452361e45a01b293781b6c5317ffbdb0cea27fb9
Nicolas Schneider <nicolas06062000@hotmail.com>
2019-01-26 19:23:45 +0100

    Add random values to objects

--- Assets/Scripts/Objects/PickableObjectData.cs
+++ Assets/Scripts/Objects/PickableObjectData.cs
@@ -8,4 +8,11 @@ public class PickableObjectData : ScriptableObject
     [SerializeField] public Vector2Int size;
     [SerializeField] public bool isPlaced = false;
     [SerializeField] public List<Vector2Int> tileTaken;
+
+    [SerializeField] [Range(-5, 5)] public float FunAmout;
+    [SerializeField] [Range(-5, 5)] public float RelaxingAmout;
+    [SerializeField] [Range(-5, 5)] public float WeirdAmout;
+    [SerializeField] [Range(-5, 5)] public float CleanAmout;
+    [SerializeField] [Range(-5, 5)] public float FoodAmout;
+    [SerializeField] [Range(-5, 5)] public float WorkAmout;
 }
