9fc752b9b852b42d161b5849850d446a7fcd2f94
Nicolas Schneider <nicolas06062000@hotmail.com>
2019-01-26 18:56:58 +0100

    Add level's limit

---
+++ Assets/FreeMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class FreeMovement : MonoBehaviour
+{
+    Rigidbody2D body;
+
+    Vector2 directionMovement;
+
+    [SerializeField] float speedMovement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        directionMovement.x = Input.GetAxis("Horizontal");
+        directionMovement.y = Input.GetAxis("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        body.velocity = directionMovement * speedMovement;
+        directionMovement = Vector2.zero;
+    }
+}
