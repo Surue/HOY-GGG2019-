d4f3e002c5fcf4260ecfb664545086d1106534b6
Nicolas Schneider <nicolas06062000@hotmail.com>
2019-01-27 14:52:47 +0100

    Add music to game

--- Assets/MusicManager.cs
+++ Assets/MusicManager.cs
@@ -1,23 +1,56 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using FMOD.Studio;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MusicManager : MonoBehaviour
 {
-    void Awake()
-    {
-        DontDestroyOnLoad(this);
+    static MusicManager _instance = null;
+    public static MusicManager Instance => (MusicManager)_instance;
+
+    protected void Awake() {
+        if(_instance == null) {
+            DontDestroyOnLoad(this);
+            _instance = this;
+        } else if(_instance != this) {
+            Destroy(gameObject);
+        }
     }
+
+    [Header("Musics")]
+    [FMODUnity.EventRef] public string musicMenu;
+    [FMODUnity.EventRef] public string musicGame;
+
+    bool isPlayingMusicGame = false;
+
+    EventInstance eventSound;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        SceneManager.sceneLoaded += OnSceneLoaded;
+
+        if(SceneManager.GetActiveScene().name == "MainMenu") {
+            eventSound.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+            eventSound = SoundManager.Instance.PlaySingle(musicMenu, transform.position);
+            isPlayingMusicGame = false;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnSceneLoaded(Scene scene, LoadSceneMode mode)
     {
-        
+        if (scene.name == "MainMenu") {
+            eventSound.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+            eventSound = SoundManager.Instance.PlaySingle(musicMenu, transform.position);
+            isPlayingMusicGame = false;
+        }
+
+        if (scene.name == "HUB" && !isPlayingMusicGame) {
+            eventSound.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+            eventSound = SoundManager.Instance.PlaySingle(musicGame, transform.position);
+            isPlayingMusicGame = true;
+        }
     }
 }
