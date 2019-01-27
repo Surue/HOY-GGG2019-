f14488ddb3659e908e94060229849f5843936151
Nicolas Schneider <nicolas06062000@hotmail.com>
2019-01-27 08:53:45 +0100

    Add wiggle effect to main menu

---
+++ Assets/Scripts/UiWiggleWotation.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions.Must;
+using UnityEngine.Experimental.UIElements;
+
+public class UiWiggleWotation : MonoBehaviour
+{
+
+    [SerializeField] float speedWiggle = 2;
+    [SerializeField] float amountWiggle = 3;
+    
+    float originRotation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        originRotation = transform.eulerAngles.z;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.rotation = Quaternion.Euler(0, 0, Mathf.Sin(Time.time * speedWiggle) * amountWiggle + originRotation);
+    }
+}
